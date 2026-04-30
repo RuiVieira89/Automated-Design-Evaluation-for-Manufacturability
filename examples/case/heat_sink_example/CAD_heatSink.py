4c2f9249e97641813bda3e9be4abd02c03c2fbeb
Rui Vieira <rui.vieira.vv@gmail.com>
2026-04-30 06:47:59 +0200

    moved FEA case

--- examples/case/heat_sink_example/CAD_heatSink.py
+++ examples/case/heat_sink_example/CAD_heatSink.py
@@ -84,7 +84,7 @@ from typing import Optional
 # ---------------------------------------------------------------------------
 # repo root on sys.path
 # ---------------------------------------------------------------------------
-ROOT = Path(__file__).resolve().parents[2]
+ROOT = Path(__file__).resolve().parents[3]
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
 
